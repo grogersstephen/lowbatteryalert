6a03a916b99e6d8d1e71120591fc58549c5bcf2c
Joseph Palacio <josephmpalacio@gmail.com>
2023-08-17 17:22:00 -0500

    just for conversation

--- main.go
+++ main.go
@@ -1,26 +1,41 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"time"
 )
 
+// NOTE: becareful of global variables. they should be read only. or they should be singletons
 const (
-	PATH     = "/sys/class/power_supply/BAT0/" // Battery information path
-	CRITICAL = iota                            // Levels for the battery
+	PATH = "/sys/class/power_supply/BAT0/" // Battery information path
+)
+
+// I would make a type alias of these. It can be easier to find and seperate concerns
+type threshold = int
+
+const (
+	CRITICAL threshold = iota // Levels for the battery
 	VERY_LOW
 	LOW
 	SAFE
 )
 
-var THRESHOLD = map[int]int{ // Battery Percentage thresholds that define the levels
+var THRESHOLD = map[threshold]int{ // Battery Percentage thresholds that define the levels
 	CRITICAL: 5,
 	VERY_LOW: 10,
 	LOW:      20,
 }
 
 func main() {
+	intervalString := flag.String("interval", "1m", "")
+	flag.Parse()
+
+	interval, err := time.ParseDuration(*intervalString)
+	if err != nil {
+		log.Fatalf("parse interval: %w", err)
+	}
 	// Declare our battery struct
 	b := battery{
 		Level: SAFE,
@@ -30,7 +45,7 @@ func main() {
 	for {
 
 		// Get battery status from file
-		err := b.getValues()
+		isCharing, charge, err := b.getValues()
 		if err != nil {
 			n := notification{
 				"-t":   "ERROR",
@@ -44,10 +59,10 @@ func main() {
 		}
 
 		// updateLevel checks to see if the battery level drops below or rises above a defined threshold
-		if b.updateLevel() == -1 && b.Charging == false { // Battery level dropped below a threshold
+		if b.updateLevel(charge) == -1 && !isCharing { // Battery level dropped below a threshold
 			n := notification{
 				"-t":    "LOW BATTERY",
-				"-c":    fmt.Sprintf("Your battery is currently at %d%%. Please provide power.", b.Charge),
+				"-c":    fmt.Sprintf("Your battery is currently at %d%%. Please provide power.", charge),
 				"-s":    "50",
 				"-d":    "5000",
 				"--fs":  "30",
@@ -60,6 +75,6 @@ func main() {
 			}
 		}
 
-		time.Sleep(time.Minute)
+		time.Sleep(interval)
 	}
 }
